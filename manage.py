a10d5e545cf6601ec7c6473ad3f7bd4056234484
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-25 04:27:15 -0500

    prep heroku push

--- manage.py
+++ manage.py
@@ -1,7 +1,14 @@
 from flask_script import Manager, Server
-from chatzone import app
+from chatzone import app, socketio
 
 manager = Manager(app)
+
+
+class Server(_Server):
+    def __call__(self):
+        socketio.run(app)
+        
+
 manager.add_command("runserver", Server())
 
 if __name__ == "__main__":
