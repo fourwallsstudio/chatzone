de6c506ecd5c7672a129cd9e45df8fe7b3ba6cc6
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-25 05:59:51 -0500

    fix typo

--- manage.py
+++ manage.py
@@ -14,7 +14,7 @@ class Server(_Server):
         
 
 manager.add_command("runserver", Server())
-manager.add_command("db", MigrateCommnad)
+manager.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
