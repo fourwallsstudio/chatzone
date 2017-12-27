e541c98ace509af1a7c1555660e92e971cabedd2
Calvin McElroy <fourwallsstudio@gmail.com>
2017-12-26 20:11:26 -0500

    remove script

--- manage.py
+++ manage.py
@@ -1,22 +1,24 @@
-from flask_script import Manager, Server as _Server
-from flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager, Server as _Server
+# from flask_migrate import Migrate, MigrateCommand
 from chatzone import app, db, socketio
-import eventlet
+# import eventlet
 # eventlet.monkey_patch()
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+
+# migrate = Migrate(app, db)
+# manager = Manager(app)
 
 
-class Server(_Server):
-    def __call__(self, app):
-        eventlet.wrap_ssl(socketio)
-        socketio.run(app)
+# class Server(_Server):
+#    def __call__(self, app):
+#        eventlet.wrap_ssl(socketio)
+#        socketio.run(app)
         
 
-manager.add_command("runserver", Server())
-manager.add_command("db", MigrateCommand)
+# manager.add_command("runserver", Server())
+# manager.add_command("db", MigrateCommand)
 
 
 if __name__ == "__main__":
-    manager.run()
+    # manager.run()
+    socketio.run(app)
